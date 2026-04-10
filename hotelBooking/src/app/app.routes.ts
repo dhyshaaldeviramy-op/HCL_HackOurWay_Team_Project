4af2db814551c04f210e31ee3159ed1b045a2742
Dhyshaal Devira <dhyshaaldevira@gmail.com>
2026-04-10 09:47:27 +0530

    complete frontend files

--- hotelBooking/src/app/app.routes.ts
+++ hotelBooking/src/app/app.routes.ts
@@ -1,17 +1,50 @@
-// import { Routes } from '@angular/router';
-
-// export const routes: Routes = [];
-
-
+import { BookingPage } from './../modules/booking/booking-page/booking-page';
+import { Login } from './../modules/auth/login/login';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { Register } from '../modules/auth/register/register';
 import { HotelList } from '../modules/hotels/hotel-list/hotel-list';
 import { HotelDetails } from '../modules/hotels/hotel-details/hotel-details';
+import { MyBookings } from '../modules/booking/my-bookings/my-bookings';
+import { PaymentComponent } from '../modules/payment/payment/payment';
+import { authGuardGuard } from '../core/guards/auth-guard-guard';
 
 const routes: Routes = [
+
+  // Default route
   { path: '', component: HotelList },
-  { path: 'hotels/:id', component: HotelDetails }
+
+  // Auth
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+
+  // Hotels
+  { path: 'hotels', component: HotelList },
+  { path: 'hotels/:id', component: HotelDetails },
+
+  // Booking
+  { 
+    path: 'booking', 
+    component: BookingPage,
+    canActivate: [authGuardGuard]   // protect route
+  },
+
+  { 
+    path: 'my-bookings', 
+    component: MyBookings,
+    canActivate: [authGuardGuard]
+  },
+
+  // Payment
+  { 
+    path: 'payment', 
+    component: PaymentComponent,
+    canActivate: [authGuardGuard]
+  },
+
+  // Wildcard (404)
+  { path: '**', redirectTo: '' }
+
 ];
 
 @NgModule({
--- hotelBooking/src/shared/components/navbar/navbar.ts
+++ hotelBooking/src/shared/components/navbar/navbar.ts
@@ -1,9 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+
+  constructor(private router: Router) {}
+
+  goHome() {
+    this.router.navigate(['/']);
+  }
+
+  goBookings() {
+    this.router.navigate(['/my-bookings']);
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+}
